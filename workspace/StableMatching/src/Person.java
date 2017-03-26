9f61743ebf0dcb4aadefa71878faeaba77405a00
Mike <animelovah12@gmail.com>
2017-03-26 15:49:21 +0200

    added project

---
+++ workspace/StableMatching/src/Person.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public abstract class Person {
+	String Name;
+	int index;
+	ArrayList<Integer> prefList;
+	Person Partner = null;
+	
+	public int getIndex() {
+		return index;
+	}
+	
+	public String getName() {
+		return Name;
+	}
+	
+	public ArrayList<Integer> getList() {
+		return prefList;
+	}
+	
+	public Person getPartner() {
+		return Partner;
+	}
+	
+	public void setPartner(Person Partner) {
+		this.Partner = Partner;
+	}
+}
+
