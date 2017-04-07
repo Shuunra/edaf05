d12aea4f88e95650687d779bdbbe8b79b6e1f135
Myky Tran <Mykytran@gmail.com>
2017-04-07 13:17:56 +0200

    Fixed initial city

--- workspace/SpanningUSA/src/SpanningMain.java
+++ workspace/SpanningUSA/src/SpanningMain.java
@@ -14,6 +14,8 @@ public class SpanningMain {
 		
 		HashMap<String, City> cities = new HashMap<String, City>();
 		ArrayList<String> cityName = new ArrayList<String>();
+		int minDist = Integer.MAX_VALUE;
+		Edge minEdgeDist = null;
 		
 		//ArrayList<City> cities = new ArrayList<City>();
 		//ArrayList<Edge> edges = new ArrayList<Edge>();
@@ -21,12 +23,14 @@ public class SpanningMain {
 		String filename = "C:\\Users\\Myky\\Documents\\edaf05_2\\algdes-labs-master\\spanning-usa\\data\\USA-highway-miles.txt";
 		//String filename = "C:\\Users\\Myky\\Documents\\edaf05_2\\algdes-labs-master\\spanning-usa\\data\\tinyEWG-alpha.txt";
 		//String filename = "C:\\Users\\Shintai\\Desktop\\edaf05\\algdes-labs-master\\spanning-usa\\data\\tinyEWG-alpha.txt";
+		//String filename = "C:\\Users\\Myky\\Documents\\edaf05_2\\algdes-labs-master\\spanning-usa\\data\\test.txt";
 		
 		try {
+			
 			BufferedReader br = new BufferedReader(new FileReader(filename));
 			String line = br.readLine().trim();
 			int i = 0;
-			while (line.charAt(line.length()-1) != ']' ) { //find metod?
+			while (line.indexOf("]") == -1) { //find metod?
 				//if (line.charAt(0) == '"' ) {
 					//line = line.substring(1, line.length() - 1);
 				//}
@@ -37,16 +41,23 @@ public class SpanningMain {
 			}
 
 			while(line != null) {
-				String[] parts = line.split("--|\\[|\\]+");//Add a + mby? Delimiters are --, ", [ and ]
+				String[] parts = line.split("--|\\[|\\]");//Add a + mby? Delimiters are --, ", [ and ]
 				String U = parts[0].trim();						 //Also problem with splitting using blank space and "
-				City b = cities.get(U);//Gotta fix getting ahold of city...
+				City a = cities.get(U);//Gotta fix getting ahold of city...
 				String V = parts[1].trim();
-				City a = cities.get(V);
+				City b = cities.get(V);
 				String dist = parts[2].trim();
 				int dis = Integer.parseInt(dist);
 				Edge E = new Edge(a,b,dis);
+				Edge E2 = new Edge(b,a,dis);
 				a.addEdge(E);
-				b.addEdge(E);
+				b.addEdge(E2);
+				
+				if (minDist > dis) {
+					minDist = dis;
+					minEdgeDist = E;
+				}
+				
 				line = br.readLine();
 				
 
@@ -62,20 +73,24 @@ public class SpanningMain {
 
 		}
 		
-		Prims(cities,cityName);
+		Prims(cities ,cityName, minEdgeDist);
 	}
 	
-		public static void Prims (HashMap<String, City> hm, ArrayList<String> cities) {
+		public static void Prims (HashMap<String, City> hm, ArrayList<String> cities, Edge minEdge) {
 			PriorityQueue<City> pq = new PriorityQueue<City>();
 			ArrayList<City> spanningTree = new ArrayList<City>();
+			
+			City a = minEdge.cityU();
+			a.setDist(0);
 			
 			for (String c : cities) {
 				City d = hm.get(c);
 				pq.offer(d);
 			}
-			City a = pq.poll();
-			a.setDist(0);
-			pq.add(a);
+			
+			//City a = pq.poll();
+			//a.setDist(0);
+			//pq.add(a);
 			
 			while (!pq.isEmpty()) {
 				City u = pq.poll();
@@ -83,18 +98,26 @@ public class SpanningMain {
 				PriorityQueue<City> temp = new PriorityQueue<City>();
 				
 				for (int i = 0; i < u.getEdges().size(); i++) {
-					if ((!spanningTree.contains(u.getEdges().get(i).cityU()) && spanningTree.contains(u.getEdges().get(i).cityV())) ||
-							(spanningTree.contains(u.getEdges().get(i).cityU()) && !spanningTree.contains(u.getEdges().get(i).cityV()))) {
-						City v;
-						if (u == u.getEdges().get(i).cityU()) {
-							v = u.getEdges().get(i).cityV();
-						} else {
-							v = u.getEdges().get(i).cityU();
-						}
-						if ((u.getEdges().get(i).getDist() < v.getDist())) {
-							v.setDist(u.getEdges().get(i).getDist());
+					Edge e = u.getEdges().get(i);
+					City v = e.cityV();
+					if (!spanningTree.contains(v)) {
+						if (e.getDist() < v.getDist()) {
+							v.setDist(e.getDist());
 						}
 					}
+					
+//					if ((!spanningTree.contains(u.getEdges().get(i).cityU()) && spanningTree.contains(u.getEdges().get(i).cityV())) ||
+//							(spanningTree.contains(u.getEdges().get(i).cityU()) && !spanningTree.contains(u.getEdges().get(i).cityV()))) {
+//						City v;
+//						if (u == u.getEdges().get(i).cityU()) {
+//							v = u.getEdges().get(i).cityV();
+//						} else {
+//							v = u.getEdges().get(i).cityU();
+//						}
+//						if ((u.getEdges().get(i).getDist() < v.getDist())) {
+//							v.setDist(u.getEdges().get(i).getDist());
+//						}
+//					}
 					
 				}
 				
