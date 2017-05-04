0406ff77e09132a5e5c7b251bff08ac9372d3dcf
Mike <animelovah12@gmail.com>
2017-05-04 14:31:32 +0200

    Continued with parser

--- workspace/Gorilla/src/TempParser.java
+++ workspace/Gorilla/src/TempParser.java
@@ -2,11 +2,13 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class TempParser {
 
 	public static void main(String[] args) {
-
+		
+		ArrayList<DNA> DNAlist = new ArrayList<DNA>();
 		String filename = "C:\\Users\\Shintai\\Desktop\\edaf05\\algdes-labs-master\\gorilla\\data\\HbB_FASTAs-in.txt";
 
 		try {
@@ -19,11 +21,12 @@ public class TempParser {
 			for (int i = 0; i < inputNbr; i++) {
 				String name = br.readLine().trim();
 				String DNA = br.readLine().trim();
+				DNAlist.add(new DNA(name, DNA));
 			}
 			for (int i = 0; i < queryNbr; i++) {
 				line = br.readLine().trim();
 				parts = line.split("\\s");
-				// Call method on parts[1] and parts[2]
+				// Call method on parts[1] and parts[2], aka. use SequenceAlignment on the string
 			}
 
 			// Input file
@@ -36,8 +39,7 @@ public class TempParser {
 			line2 = br.readLine().trim();
 			while (line2 != null) {
 				if (line2.charAt(0) == '>') {
-					DNA obj = new DNA(name2, sb.toString());
-					//Add to list with all species
+					DNAlist.add(new DNA(name2, sb.toString()));		//Add to list with all species
 					parts2 = line2.split("\\s");
 					name2 = parts2[0].substring(1).trim();
 					line2 = br.readLine().trim();
@@ -45,6 +47,15 @@ public class TempParser {
 				} else {
 					sb.append(line2);
 					line2 = br.readLine().trim();
+				}
+			}
+			
+			for(int i = 0; i < DNAlist.size() - 1; i++) {
+				for(int j = i + 1; j < DNAlist.size(); j++) {
+					//SequenceAlignment between i and j, print in format:
+					//Species1--Species2: score
+					//Species1DNA
+					//Species2DNA
 				}
 			}
 
